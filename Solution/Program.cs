2ce54a8ad17d7331fcfd8be0f492f743df493aac
orangenaste <orangenaste@gmail.com>
2023-01-13 12:11:42 +0300

    Add solution of the task

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -8,3 +8,34 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Write("Enter array's size: ");
+int size = int.Parse(Console.ReadLine());
+
+string[] array = Array(size);
+string[] Array(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Enter {i + 1}/{size} element of array: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+int maxStringLength = 3;
+
+string[] arrayCut = maxStringLength3(size);
+string[] maxStringLength3(int size)
+{
+    string[] arrayCut = new string[size];
+    for (int i = 0; i < arrayCut.Length; i++)
+    {
+        if (array[i].Length <= maxStringLength)
+            arrayCut[i] = array[i];
+    }
+    return arrayCut;
+}
+
+Console.WriteLine($"A new array of strings with length less than or equal to {maxStringLength}:[{String.Join(" ", arrayCut)}]");
